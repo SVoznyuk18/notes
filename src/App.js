9798e215360bc43a41cdad1bdfd4787f450b8f0b
Sergey.V <s.voznyuk18@gmail.com>
2023-05-06 21:17:38 +0300

    add Context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Context from './Context';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ test: 2 }}>
+      <div className="App">
+   
+        <Sidebar />
+      </div>
+
+    </Context.Provider>
+
   );
 }
 
