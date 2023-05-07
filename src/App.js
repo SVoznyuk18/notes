2e289ade05afd2f821292a2d509e7870ec5748cf
Sergey.V <s.voznyuk18@gmail.com>
2023-05-07 17:49:20 +0300

    create format date func

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Context from './Context';
 
 import { Toolsbar, Sidebar } from 'ComponentsRoot';
+import {formatDate, formatFullDate} from 'UtilsRoot';
 // import iconSvg from 'AssetsRoot/svg/iconSvg';
 // import { colors } from "ConfigsRoot/colors";
 
 import { Wrapper } from './StyledComponents';
 
 function App() {
+
   return (
     <Context.Provider value={{ test: 2 }}>
       <div className="App">
---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+
+
+export const formatDate = (noteDate) => {
+    const dayMs = 24*60*60*1000;
+    const currentDate = Date.now();
+    let formatedDate = '';
+
+    if((currentDate - noteDate) > dayMs) {
+        formatedDate = moment(noteDate).format('MM/DD/YY');
+    } else {
+        formatedDate = moment(noteDate).format('h:mm a');
+    }
+
+    return formatedDate;
+}
+
+export const formatFullDate = (noteDate) => {
+    const formatedFullDate = moment(noteDate).format('MMMM Do YYYY, h:mm a');
+    return formatedFullDate;
+}
