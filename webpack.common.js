72c839ed0df9ba98e40f66c67fcf4f6c377dd6e8
Sergey.V <s.voznyuk18@gmail.com>
2023-05-10 15:06:12 +0300

    delete exrtra alias from webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -9,8 +9,6 @@ module.exports = {
       ComponentsRoot: path.resolve(__dirname, './src/components'),
       AssetsRoot: path.resolve(__dirname, './src/assets'),
       UtilsRoot: path.resolve(__dirname, './src/utils'),
-      LayoutRoot: path.resolve(__dirname, './src/layout'),
-      ContainersRoot: path.resolve(__dirname, './src/containers')
     },
     extensions: ['.js', '.jsx', '.json']
   },
